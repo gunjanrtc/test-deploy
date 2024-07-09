34acac253dac8fcecb56bf2956a543fbe04cd4b0
gunjanrtc <156164197+gunjanrtc@users.noreply.github.com>
2024-07-09 12:44:34 +0530

    test

---
+++ .buildkite/pipeline.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+SERVICE=$(buildkite-agent meta-data get SERVICE)
+
+json_file="C:\\buildkite-agent\\builds\\RTC-DLLA48-1\\rtctek\\test-deploy\\.buildkite\\services.json"
+
+get_service_details() {
+    local service_name=$1
+    local detail=$2
+    jq -r --arg service "$service_name" --arg detail "$detail" '.[$service][$detail]'  $json_file
+}

9097b84f33fb5aa8c661e3142e89cae99cd10109
gunjanrtc <156164197+gunjanrtc@users.noreply.github.com>
2024-07-09 15:25:59 +0530

    test

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -1,22 +1,10 @@
 #!/usr/bin/env bash
 
-# Function to retrieve service names from Buildkite metadata
-get_service_names() {
-    local services=$(buildkite-agent meta-data get SERVICE)
-    echo "$services"
-}
+SERVICE=$(buildkite-agent meta-data get SERVICE)
 
-# Example usage of the function
-service_names=$(get_service_names)
+echo "$SERVICE"
 
-# Print service names for verification
-echo "Service names: $service_names"
 
-# Upload service names to Buildkite as pipeline metadata
-buildkite-agent pipeline upload <<EOF
-{
-  "env": {
-    "SERVICE_NAMES": "$service_names"
-  }
-}
-EOF
+for service_name in $SERVICE; do
+    echo "service name = $service_name"
+done

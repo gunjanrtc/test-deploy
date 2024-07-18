93bedd5a342d6dc7396fa05624b3375596bd5899
gunjanrtc <156164197+gunjanrtc@users.noreply.github.com>
2024-07-18 16:02:56 +0530

    test

--- .buildkite/setup.sh
+++ .buildkite/setup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+SERVICES=$(buildkite-agent meta-data get SERVICE)
+echo "all services are $SERVICES"
 
 # Fetch the selected services from the input
 IFS=', ' read -ra SERVICES <<< "$(buildkite-agent meta-data get SERVICE)"
 echo "All : $SERVICES"
+
 # Loop through each service and export it as an environment variable
 for SERVICE in "${SERVICES[@]}"; do
   export SERVICE_NAME="$SERVICE"
